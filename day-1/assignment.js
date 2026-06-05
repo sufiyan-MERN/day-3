ade069a6a6102442a5e5989cf46279e3b815154a
syed sufiyan ali <sufiyantdc@gmail.com>
2026-06-05 22:44:33 +0530

    js practice

---
+++ day-1/assignment.js
@@ -0,0 +1,46 @@
+let name = "sufiyan";
+console.log(name);
+
+age = 19;
+console.log(age);
+
+student = true;
+console.log(student);
+
+color = "green";
+console.log(color);
+
+sibilings = 3;
+console.log(sibilings);
+
+let score;
+console.log(score);
+let wintter = null;
+console.log(wintter);
+
+firstname = "syed";
+console.log(firstname);
+
+lastname = "ali";
+console.log(lastname);
+
+fullname = firstname + lastname;
+console.log(fullname);
+
+num1 = 45;
+num2 = 12;
+sum = num1 + num2;
+difference = num1 - num2;
+product = num1 * num2;
+quotient = num1 / num2;
+remainder = num1 % num2;
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
+
+celsius = 25;
+farenheit = (celsius * 9) / 5 + 32;
+result = farenheit;
+console.log("25°C is equal to " + result + "°F");
---
+++ day-1/script.js
@@ -0,0 +1,9 @@
+// let agee = 23;
+// let username = "sufiyan";
+
+// console.log(username);
+// console.log(agee);
+
+// sum = 5 + 3;
+
+// console.log(sum);
